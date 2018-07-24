42a2425be6a4986b9b0fbd9cffc6bb2aa35cf281
Sebastian Asp <sebastian@sjundegatan.se>
2018-07-24 12:52:08 +0200

    Initial Commit

---
+++ Goolf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goolf
+//
+//  Created by Sebastian Asp on 2018-07-24.
+//  Copyright © 2018 Sebastian Asp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
