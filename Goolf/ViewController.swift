d40ed0f85141658d4088ab45f9a58c1a08a61018
Sebastian Asp <sebastian@sjundegatan.se>
2018-07-25 11:41:34 +0200

    Added labels, api, ui

--- Goolf/ViewController.swift
+++ Goolf/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+
+    let locationManager = CLLocationManager()
+    var currentLocation = CLLocation()
+    
+    var currentWeather: CurrrentWeather!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+        currentWeather = CurrrentWeather()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationAuthStatus()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            currentLocation = locationManager.location!
+            Location.sharedInstance.latitude = currentLocation.coordinate.latitude
+            Location.sharedInstance.longitude = currentLocation.coordinate.longitude
+            currentWeather.downloadWeatherDetails {
+                
+                DispatchQueue.main.async {
+                    self.updateMainUI()
+                    
+                }
+                
+            }
+        } else {
+            
+            locationManager.requestWhenInUseAuthorization()
+            
+        }
+    }
+    
+    func updateMainUI() {
+        dateLabel.text = currentWeather.date
+        currentTempLabel.text = "\(currentWeather.currentTemp)"
+        locationLabel.text = currentWeather.cityName
+        currentWeatherImage.image = UIImage(named: currentWeather.weatherType)
+    }
+    
+   
+    
 
+    @IBAction func getLocationBtn(_ sender: Any) {
+    }
+    
 }
 
